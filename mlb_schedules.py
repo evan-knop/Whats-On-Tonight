d937ba46b0d8a3c9da06e6c197f69c5a1e655003
evan-knop <eknop23@gmail.com>
2025-07-28 14:44:59 -0500

    started work on mlb schedule scraping

--- mlb_schedules.py
+++ mlb_schedules.py
@@ -3,3 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 base_url = "https://www.baseball-reference.com/"
+schedule_url = "https://www.baseball-reference.com/leagues/MLB-schedule.shtml"
+
+response = requests.get(schedule_url)
+soup = BeautifulSoup(response.text, 'html.parser')

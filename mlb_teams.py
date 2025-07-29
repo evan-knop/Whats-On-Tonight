e967dd8b8e3f63758fb330bede672864b4a957d9
evan-knop <eknop23@gmail.com>
2025-07-28 20:14:02 -0500

    planned out columns needed for Teams dimension

--- mlb_teams.py
+++ mlb_teams.py
@@ -1,22 +1,19 @@
-import requests, time
-import pandas as pd
+import requests
 
 url = (
-    "https://statsapi.mlb.com/api/v1/schedule"
+    "https://statsapi.mlb.com/api/v1/teams"
     "?sportId=1"
-    "&startDate=2025-03-29"
-    "&endDate=2025-09-29"
 )
 
 resp = requests.get(url)
 data = resp.json()
 
 # Example: list dates and matchups
-for date in data.get("dates", []):
+for date in data.get("teams", []):
     print(date["date"])
     for game in date["games"]:
         away = game["teams"]["away"]["team"]["name"]
         home = game["teams"]["home"]["team"]["name"]
         print(f"  {away} at {home}")
 
-        #
+        #id, abbreviation, teamName, locationName, league[id], league[name], divistion[id,] division[name]

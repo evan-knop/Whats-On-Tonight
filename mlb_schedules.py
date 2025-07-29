fb2c56be71dfb8c9103dd4f36063e51eb68632ff
evan-knop <eknop23@gmail.com>
2025-07-28 20:00:46 -0500

    changed to use mlb api instead of beautiful soup

--- mlb_schedules.py
+++ mlb_schedules.py
@@ -1,9 +1,20 @@
 import requests, time
 import pandas as pd
-from bs4 import BeautifulSoup
 
-base_url = "https://www.baseball-reference.com/"
-schedule_url = "https://www.baseball-reference.com/leagues/MLB-schedule.shtml"
+url = (
+    "https://statsapi.mlb.com/api/v1/schedule"
+    "?sportId=1"
+    "&startDate=2025-03-29"
+    "&endDate=2025-09-29"
+)
 
-response = requests.get(schedule_url)
-soup = BeautifulSoup(response.text, 'html.parser')
+resp = requests.get(url)
+data = resp.json()
+
+# Example: list dates and matchups
+for date in data.get("dates", []):
+    print(date["date"])
+    for game in date["games"]:
+        away = game["teams"]["away"]["team"]["name"]
+        home = game["teams"]["home"]["team"]["name"]
+        print(f"  {away} at {home}")
